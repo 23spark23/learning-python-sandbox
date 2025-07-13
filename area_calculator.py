8b9490ed5afa6d963de71541ad2a3835348228a9
23spark23 <andrei.catana2308@gmail.com>
2025-07-13 08:23:09 +0300

    Create area_calculator.py

---
+++ area_calculator.py
@@ -0,0 +1,27 @@
+# Hello there!
+# This program will calculate the area of a triangle, rectangle and circle.
+
+import math
+
+# Calculating the area of a triangle:
+
+base = 20
+height = 30
+area = base * height / 2
+
+print("The triangle area is", area)
+
+# Calculcating the area of a rectangle:
+
+length = 2
+width = 12
+area = length * width
+
+print("The rectangle area is", area)
+
+#Calculating the area of a circle:
+
+radius = 36
+area = math.pi * radius * radius
+    
+print("The circle area is", area)
