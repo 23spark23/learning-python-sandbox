b3fc9d97547488ca73d6a91c5a76f1dc2855b372
23spark23 <andrei.catana2308@gmail.com>
2025-07-13 08:14:09 +0300

    Create fortune_cookie_program.py

---
+++ fortune_cookie_program.py
@@ -0,0 +1,49 @@
+# Hello there!
+# This is a Fortune Cookie program that is supposed to print out a possible fortune based on a random number and an if/elif statement.
+
+import random
+
+fortune = random.randint(0, 20)
+
+if fortune == 0:
+  print("May you one day be carbon neutral")
+elif fortune == 1:  
+  print("You have rice in your teeth")
+elif fortune == 2:
+  print("No snowflake feels responsible for an avalanche")
+elif fortune == 3:
+  print("You can only connect the dots looking backwards")
+elif fortune == 4:
+  print("The fortune you seek is in another cookie")
+elif fortune == 5:  
+  print("Stay at home today")
+elif fortune == 6:
+  print("Because you demand more from yourself, others respect you deeply.")
+elif fortune == 7:
+  print("The value lies not within any particular thing, but in the desire placed on that thing.")
+elif fortune == 8:
+  print("You have a friendly heart and are well admired.")
+elif fortune == 9:  
+  print("Nothing is more difficult, and therefore more precious, than to be able to decide.")
+elif fortune == 10:
+  print("You should go at that concert you always wanted to go at")
+elif fortune == 11:
+  print("Text them, it is never too late")
+elif fortune == 12:
+  print("Trust your instincts")
+elif fortune == 13:  
+  print("A smile is the universal language of kindness. Share it often:)")
+elif fortune == 14:
+  print("Embrace change, it will lead you to unexpected adventures!")
+elif fortune == 15:
+  print("A ship in harbor is safe; but that is not what ships are built for;)")
+elif fortune == 16:
+  print("You will find success and joy in unexpected places.")
+elif fortune == 17:  
+  print("Consolidate rather than expand your business in the near future.")
+elif fortune == 18:
+  print("You will have a fine capacity for the enjoyment of life.")
+elif fortune == 19:
+  print("Good fortune awaits you at the end of the day.")
+elif fortune == 20:
+  print("You have an active mind and a keen imagination.")
